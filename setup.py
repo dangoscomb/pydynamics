587740f169014bb514eb9733f9f30c706bd79c87
Dan Goscomb <dan.goscomb@hso.co.uk>
2018-12-29 21:55:18 +0000

    fix project url in setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Talk to On-Prem Dynamics CRM",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/dangoscomb/pydynamics",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
